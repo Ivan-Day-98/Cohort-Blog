acc97be9b4ba824b5c2aaeaa7f7950d95fbe9747
Ivan-Day-98 <ivan.day@koddi.com>
2023-06-13 15:48:06 -0500

    Add email private variable to user

--- src/Blog/User.php
+++ src/Blog/User.php
@@ -4,6 +4,7 @@
     class User{
         private $username;
         private $password;
+        private $email;
         public function __construct($username, $password, $email){
             $this->username = $username;
             $this->password = $password;
